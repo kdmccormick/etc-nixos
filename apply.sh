a5a7bbe511ea093892a2e5a746160d7a27e5606a
Kyle D McCormick <kyle@axim.org>
2025-04-04 11:44:09 -0400

    simplify: entire dir gets linked to /etc/nixos

--- apply.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Needs to be run as root
-
-set -euxo pipefail
-cp -f "/home/kyle/nix/etc-nixos/"* /etc/nixos
-#cp -f "/home/kyle/nix/home-kyle-config-nixpkgs/"* /home/kyle/.config/nixpkgs
-nixos-rebuild switch
